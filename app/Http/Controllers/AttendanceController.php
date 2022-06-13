307996f200078788381f4e9120d3b339b6ac5176
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-13 13:57:40 +0100

    Created Attendance Model with CRUD controller and Tests.

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAttendanceRequest;
+use App\Http\Requests\UpdateAttendanceRequest;
+use App\Models\Attendance;
+
+class AttendanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAttendanceRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttendanceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attendance $attendance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAttendanceRequest  $request
+     * @param  \App\Models\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttendanceRequest $request, Attendance $attendance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attendance $attendance)
+    {
+        //
+    }
+}
