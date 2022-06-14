46bed59ad9b310af1654ebc0c7759f3ac4cf229c
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-14 02:17:54 +0100

    Added InstallationController method and Tests.

--- app/Http/Controllers/InstallationController.php
+++ app/Http/Controllers/InstallationController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gym;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 
 class InstallationController extends Controller
 {
+    public function __construct()
+    {
+        // Not Found page if the app already installed
+
+        if (Config::get('APP_INSTALLED')) {
+            abort(404);
+        }
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +26,7 @@ class InstallationController extends Controller
      */
     public function index()
     {
-
+        return response()->json(Config::get('APP_INSTALLED', false));
     }
 
     /**
@@ -24,6 +37,78 @@ class InstallationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // store database credentials
+        $this->storeDatabaseCredentials($request->database);
+
+        // store gym name
+        $gym = $this->storeGymName($request->gym);
+
+        // create new role
+        $role = $this->createAdminRole();
+
+        // add gym_id to the admin array
+        $request->merge([
+            'admin' =>
+                array_push($request->admin, [
+                    'gym_id' => $gym->id
+                ])
+        ]);
+
+        dd($request->admin);
+
+        // store admin credentials
+        $admin = $this->storeAdminCredentials($request->admin);
+
+        // assign a role for the created admin
+        $this->assignAdminRole($role, $admin);
+
+
+    }
+
+    /**
+     * store database credentials in the env file
+     * @param array $credentials
+     */
+    public function storeDatabaseCredentials(array $credentials)
+    {
+
+    }
+
+    /**
+     * store gym's name
+     * @param array $gym
+     *
+     * @return Gym
+     */
+    public function storeGymName(array $gym)
+    {
+
+    }
+
+    /**
+     * create admin role
+     * @return Role
+     */
+    public function createAdminRole()
+    {
+
+    }
+
+    /**
+     * Store admin credentials
+     *
+     * @return User
+     */
+    public function storeAdminCredentials(array $admin)
+    {
+
+    }
+
+    /**
+     * Assign created role to the admin
+     */
+    public function assignAdminRole(Role $role, User $admin)
+    {
+
     }
 }
--- tests/Feature/Http/Controllers/InstallationControllerTest.php
+++ tests/Feature/Http/Controllers/InstallationControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Config;
 use Tests\TestCase;
 
 class InstallationControllerTest extends TestCase
@@ -18,5 +19,75 @@ class InstallationControllerTest extends TestCase
         $response = $this->get(route('install.index'));
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * GET /install return true if not installed
+     */
+    public function test_get_install_return_false_if_app_not_installed()
+    {
+        $http_response_header = $this->get(route('install.index'));
+
+        // TODO: use a better way to check the state
+        $this->assertTrue($http_response_header->content() == 'false');
+    }
+
+    /**
+     * install routes won't work if the app already installed
+     * assert error 404 [NOT FOUND]
+     */
+    public function test_install_routes_not_working_if_app_installed()
+    {
+
+        Config::set('APP_INSTALLED', true);
+
+        $http_response_header = $this->get(route('install.index'));
+
+        $http_response_header->assertNotFound();
+    }
+
+    /**
+     * System won't work if the app not installed
+     */
+    // TODO: work in this situation
+    public function system_not_working_if_the_app_not_installed()
+    {
+        Config::set('APP_INSTALLED', false);
+
+        $http_response_header = $this->get('/');
+
+        $http_response_header->assertForbidden();
+    }
+
+    /**
+     * Test to create a new app
+     */
+    public function test_create_new_app()
+    {
+        $database = [
+            'db_name' => 'gymsystem',
+            'db_host' => 'localhost',
+            'db_port' => 3306,
+            'username' => 'dev',
+            'password' => 'password'
+        ];
+
+        $gym = ['name' => 'gymsystem'];
+
+        $admin = [
+            'full_name' => 'Oussama ELJABBARI',
+            'email' => 'contact@ojpro.me',
+            'password' => 'password'
+        ];
+
+        $mix = [
+            'database' => $database,
+            'gym' => $gym,
+            'admin' => $admin
+        ];
+
+        $http_response_header = $this->post(route('install.store'), $mix);
+
+        $http_response_header->assertJsonStructure(['success']);
     }
 }
