8b521d64379222954bf4a057dc855cb10e65f7c4
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-15 22:43:09 +0100

    Used a guard to restrict access to install routes if the app installed or access other routes if not installed.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,7 +16,23 @@ export default {
     }
 
     //TODO: use guard for install routes
-  }
+    //TODO: check before enter any route either on url changed manually
+    this.$router.beforeEach((to, from, next) => {
+      if (!to.matched.some(record => record.meta.install)) {
+        if (this.$store.state.installed) {
+          next()
+          return
+        }
+        next('/install')
+      } else {
+        if (!this.$store.state.installed) {
+          next()
+          return
+        }
+        next('/')
+      }
+    })
+  },
 }
 
 </script>
