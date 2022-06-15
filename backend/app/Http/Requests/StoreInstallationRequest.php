59f49f9baf19a8b3930da16e44972d4fdbf62e1e
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-15 11:14:16 +0100

    Added Request validation for database configuration routes.

---
+++ backend/app/Http/Requests/StoreInstallationRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Config;
+
+class StoreInstallationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Config::get('APP_INSTALLED') == false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'DB_HOST' => ['required', 'string'],
+            'DB_PORT' => ['numeric', 'min:1024', 'max:65535'],
+            'DB_DATABASE' => ['required', 'string'],
+            'DB_USERNAME' => ['required', 'string'],
+            'DB_PASSWORD' => ['string'],
+        ];
+    }
+
+    /**
+     * Define Error Messages
+     */
+    public function attributes()
+    {
+        return [
+            'DB_HOST' => 'database url',
+            'DB_PORT' => 'database port',
+            'DB_DATABASE' => 'database name',
+            'DB_USERNAME' => 'database username',
+            'DB_PASSWORD' => 'database password'
+        ];
+    }
+}
