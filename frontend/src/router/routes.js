e91ef28c30e11d03e587e194777469e0cd2e4263
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-15 22:39:53 +0100

    Used Custom guard to handle install routes access protection.

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -9,26 +9,41 @@ const routes = [
         path: '/install',
         name: 'install.start',
         component: () => import('../views/install/Index.vue'),
+        meta: {
+            install: true
+        }
     },
     {
         path: '/install/database',
         name: 'install.database',
         component: () => import('../views/install/Database.vue'),
+        meta: {
+            install: true
+        }
     },
     {
         path: '/install/gym',
         name: 'install.gym',
         component: () => import('../views/install/GYM.vue'),
+        meta: {
+            install: true
+        }
     },
     {
         path: '/install/admin',
         name: 'install.admin',
         component: () => import('../views/install/Admin.vue'),
+        meta: {
+            install: true
+        }
     },
     {
         path: '/install/completed',
         name: 'install.completed',
         component: () => import('../views/install/Completed.vue'),
+        meta: {
+            install: true
+        }
     }
 ]
 
