14c4fac9d1c0d2745b2479ba5d7cf8e584482c37
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-15 14:58:00 +0100

    imported state file from the new location.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import store from './store'
+import store from './state/store'
 
 // Imported css file [TailwindCSS Behind the scene]
 import './index.css'
