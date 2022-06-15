170e9f4852360986705bb98204f5a55749888e07
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-15 14:59:16 +0100

    Dispatch fetch Installation State action.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view></router-view>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+  mounted() {
+    // fetch the installation state
+    this.$store.dispatch('fetchInstallState');
+  }
+}
+
+</script>
