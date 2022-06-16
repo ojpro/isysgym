6f2a284d3e4030ad752d8020b73c869021f9d039
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-16 13:31:07 +0100

    Used localForage instead of localStorage for saving installed state of Vuex.

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,21 +1,26 @@
 import Vuex from 'vuex'
 import {axios} from "../services/axios/config";
+import localforage from 'localforage'
 
 const store = new Vuex.Store({
     state: {
         // State to check if the app already installed
-        installed: JSON.parse(localStorage.getItem('installed'))
+        installed: await localforage.getItem('installed') || false
     },
     mutations: {
         // Set the installation state
-        setInstallState: (state, payload) => localStorage.setItem('installed',payload)
+        setInstallState(state, payload) {
+            localforage.setItem('installed', payload).then(data => {
+                state.installed = payload
+            })
+        }
     },
     actions: {
         fetchInstallState: ({commit}) => {
             // Fetch Installation State from the Backend
-            axios.get('/install').then(({data}) => {
+            axios.get('/install').then(_ => {
                 // If there is response that's mean that the app not installed yet
-                commit('setInstallState', data);
+                commit('setInstallState', false);
 
             }).catch(({response}) => {
                 // other ways if is it a 403 that's mean the app already installed.
