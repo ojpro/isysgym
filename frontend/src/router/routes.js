561f06de854cb679dcf7331d01b61a401add071f
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-16 23:34:19 +0100

    Created a dashboard route.

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -3,7 +3,12 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: () => import('../views/Home.vue')
+        component: () => import('@/views/Home.vue')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/Dashboard.vue')
     },
     {
         path: '/install',
