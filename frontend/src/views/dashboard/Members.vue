2a864f10457cf60288cae2aca2a0bf626b3a763b
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-17 19:43:01 +0100

    Created members component.

---
+++ frontend/src/views/dashboard/Members.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <DashboardLayout>
+
+    members
+
+  </DashboardLayout>
+
+</template>
+
+<script>
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+
+export default {
+  name: "Members",
+  components: {
+    'DashboardLayout': DashboardLayout
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
