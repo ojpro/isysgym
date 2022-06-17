9a7204deef7de6555057a4413a7c5a870b76bdf1
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-17 01:39:56 +0100

    Created dashboard page.

---
+++ frontend/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard-layout>
+    <div class="shadow-sm p-4 rounded bg-white">
+      This is the dashboard page
+    </div>
+  </dashboard-layout>
+</template>
+
+<script>
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+
+export default {
+  name: "Dashboard",
+  components:{
+    'dashboard-layout':DashboardLayout
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
