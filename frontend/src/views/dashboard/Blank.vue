3072c2022e6abb1ba8651a18d7a3335761c887dc
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-17 01:40:34 +0100

    Created blank page for testing.

---
+++ frontend/src/views/dashboard/Blank.vue
@@ -0,0 +1,20 @@
+<template>
+  <dashboard-layout>
+    hey
+  </dashboard-layout>
+</template>
+
+<script>
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+
+export default {
+name: "Blank",
+  components:{
+  'dashboard-layout':DashboardLayout
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
