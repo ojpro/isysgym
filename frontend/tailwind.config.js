4ce9a8394076afd509da4342211b0233afc122e0
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-17 09:36:30 +0100

    Removed flowbite from tailwind.config.js.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
