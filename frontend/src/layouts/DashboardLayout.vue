bbda1d302e4888c10ce63e0b7d95d7a51553b7d9
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-17 01:39:45 +0100

    Created dashboard layout.

---
+++ frontend/src/layouts/DashboardLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-start">
+    <Sidebar/>
+
+    <div class="w-full bg-gray-100 min-h-screen">
+      <Header/>
+      <div class="p-4">
+        <!--    Dashboard Content    -->
+        <slot/>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/partials/Header.vue";
+import Sidebar from "@/partials/Sidebar.vue";
+
+export default {
+  name: "DashboardLayout",
+  components: {
+    'Sidebar': Sidebar,
+    'Header': Header
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
