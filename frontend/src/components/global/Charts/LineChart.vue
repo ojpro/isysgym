44d9d18ac075221503248b20af0ad8cc5c92a914
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-19 14:12:30 +0100

    some chart components.

---
+++ frontend/src/components/global/Charts/LineChart.vue
@@ -0,0 +1,84 @@
+<template>
+  <Line
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :chart-options="chartOptions"
+      :css-classes="cssClasses"
+      :height="height"
+      :plugins="plugins"
+      :styles="styles"
+      :width="width"
+  />
+</template>
+
+<script>
+import {CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip} from 'chart.js'
+
+import {Line} from 'vue-chartjs'
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale
+)
+
+export default {
+  name: "LineChart",
+  components: {
+    Line
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'line-chart'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {
+      }
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 80, 40]
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/global/Charts/PieChart.vue
@@ -0,0 +1,77 @@
+<template>
+  <Pie
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :chart-options="chartOptions"
+      :css-classes="cssClasses"
+      :height="height"
+      :plugins="plugins"
+      :styles="styles"
+      :width="width"
+  />
+</template>
+
+<script>
+import {ArcElement, CategoryScale, Chart as ChartJS, Legend, Title, Tooltip} from 'chart.js'
+import {Pie} from 'vue-chartjs'
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
+
+export default {
+  name: "PieChart",
+
+  components: {
+    Pie
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'pie-chart'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {
+      }
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+        datasets: [
+          {
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+            data: [40, 20, 80, 10]
+          }
+        ]
+      },
+
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
